1fc7bd3d1456a239128e1155293112c3dc927dc4
Kenan Ölmezoglu <kenan.olmezoglu@gmail.com>
2023-06-01 23:03:26 +0200

    fix send method to resolve injected dependencies in handlers

--- MediatrLight/MediatrLight.cs
+++ MediatrLight/MediatrLight.cs
@@ -9,30 +9,24 @@ namespace MediatrLight;
 
 public class MediatrLight : IMediatrLight
 {
-    public Dictionary<Type, Type> IRequestDict { get; set; }
-    public MediatrLight(Dictionary<Type, Type> iRequestDict)
+    private readonly IServiceProvider _serviceProvider;
+    private Dictionary<Type, Type> IRequestDict { get; set; }
+    public MediatrLight(Dictionary<Type, Type> iRequestDict, IServiceProvider serviceProvider)
     {
         IRequestDict = iRequestDict;
+        _serviceProvider = serviceProvider;
     }
-    public Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default)
+    public async Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default)
     {
         var requestType = request.GetType();
 
+        if (!IRequestDict.ContainsKey(requestType))
+            throw new ArgumentException("The requested handled could not be found.");
+
         // Try to get the handler type for the request type
-        if (IRequestDict.TryGetValue(requestType, out var handlerType))
-        {
-            // Create an instance of the handler
-            var handlerInstance = Activator.CreateInstance(handlerType);
+        IRequestDict.TryGetValue(requestType, out var handlerType);
+            var handler = _serviceProvider.GetService(handlerType);
 
-            // Invoke the handler's Handle method
-            var handleMethod = handlerType.GetMethod("Handle");
-            var result = (Task<TResponse>)handleMethod.Invoke(handlerInstance, new object[] { request, cancellationToken });
-
-            return result;
-        }
-        else
-        {
-            throw new InvalidOperationException($"No handler found for request type: {requestType}");
-        }
+        return await ((IRequestHandler<IRequest<TResponse>, TResponse>)handler).Handle(request);
     }
 }
